8bf954b49d24855f775032f3c0713f25e22c75fd
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-11-06 10:48:21 -0800

    started database stuff

--- data_collection/index.js
+++ data_collection/index.js
@@ -4,10 +4,16 @@ const { getPlaylists, getPlaylistTracks } = require('./getData.js');
 const { prettyPrintPlaylist } = require('./logging.js');
 
 const trackPrintString = "Retrieved tracks for ";
+const dbclient = new Client({ database: 'spotifynchill' });
 
 const main = async function () {
   await authorization(); // cache auth token for later
+  await dbclient.connect();
+  await dbclient.query('CREATE TABLE IF NOT EXISTS Users( username CHAR(20), PRIMARY KEY (username) )');
+  await dbclient.query('CREATE TABLE IF NOT EXISTS Playlists( name CHAR(30), owner CHAR(20) REFERENCES Users ON DELETE CASCADE, PRIMARY KEY (name, owner) )');
+  //await dbclient.query('CREATE TABLE IF NOT EXISTS Tracks(  )');
   getPlaylistsAndTracks('fifi-reid');
+  dbclient.end();
 }
 
 const getPlaylistsAndTracks = async function (username) {
